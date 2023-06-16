e5e59f683fecce9d2e6682695d78c80dfc5aebf8
Akhil Nedunuri <39586134+AkhilNed@users.noreply.github.com>
2023-06-16 15:52:57 +0000

    Pending changes exported from your codespace

--- datahub-web-react/craco.config.js
+++ datahub-web-react/craco.config.js
@@ -14,6 +14,16 @@ function addLessPrefixToKeys(styles) {
 }
 
 module.exports = {
+    style: {
+        sass: {
+          loaderOptions: {
+            // Prefer 'sass' (dart-sass) over 'node-sass' if both packages are installed.
+            implementation: require('sass'),
+            // Workaround for this bug: https://github.com/webpack-contrib/sass-loader/issues/804
+            webpackImporter: false,
+          },
+        },
+    },
     webpack: {
         plugins: {
             add: [
